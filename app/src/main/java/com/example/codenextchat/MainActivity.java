fe601739b0df7ff57bc05fd569be75d5aa39e896
CodeNextSam <52679947+sameetasatya@users.noreply.github.com>
2020-05-15 01:56:05 -0700

    New Design

--- app/src/main/java/com/example/codenextchat/MainActivity.java
+++ app/src/main/java/com/example/codenextchat/MainActivity.java
@@ -6,14 +6,17 @@ import android.view.View;
 import android.widget.ArrayAdapter;
 import android.widget.EditText;
 import android.widget.ListView;
+import android.widget.TextView;
 
 import java.util.ArrayList;
 
 public class MainActivity extends AppCompatActivity {
 
     EditText editText;
+    TextView welcomeMsg;
     ArrayList<String> messages = new ArrayList<>();
     ArrayAdapter<String> messagesAdapter;
+    String userVal;
 
     //TODO: Define a DatabaseReference reference to the messages object here
 
@@ -23,7 +26,14 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        welcomeMsg = findViewById(R.id.welcomeTV);
         editText = findViewById(R.id.edit_text);
+
+        //Generates a user identification number and sets the welcome message at the top of the app
+        int userNum = (int) (Math.random()*100);
+        userVal = "User #" + userNum + ": ";
+        welcomeMsg.setText("Welcome " + userVal);
+
 
         //TODO: Place a FirebaseDatabase reference to the database here
 
@@ -50,7 +60,7 @@ public class MainActivity extends AppCompatActivity {
     public void sendMessage(View view) {
         //get the string from the edittext field
         //push the message as a child to the messages object in the db
-        String message = editText.getText().toString();
+        String message = userVal + editText.getText().toString();
         editText.setText("");
         messages.add(message); //TODO: remove later
         messagesAdapter.notifyDataSetChanged(); //TODO: remove later
